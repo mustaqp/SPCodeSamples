39e184640ec917e34ed2e4c21beffe0782bdaf71
mustaqp <36017570+mustaqp@users.noreply.github.com>
2018-02-16 00:25:51 +0000

    Update GetAllClientSecrets.ps1

--- GetAllClientSecrets.ps1
+++ GetAllClientSecrets.ps1
@@ -1,8 +1,31 @@
-﻿# Need Azure Active Directory Powershell Module
-#https://support.office.com/en-us/article/Connect-PowerShell-to-Office-365-services-06a743bb-ceb6-49a9-a61d-db4ffdf54fa6
+<# 
+Code Example Disclaimer:
+Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment. 
+THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED 'AS IS'-This is intended as a sample of how code might be written 
+for a similar purpose and you will need to make changes to fit to your requirements. 
+-This code has not been tested. This code is also not to be considered best practices or prescriptive guidance.
+-No debugging or error handling has been implemented.
+-It is highly recommended that you FULLY understand what this code is doing  and use this code at your own risk.
 
-#Also Need Online Sign in assistant
-#https://www.microsoft.com/en-us/download/details.aspx?id=39267
+Need Azure Active Directory Powershell Module
+https://support.office.com/en-us/article/Connect-PowerShell-to-Office-365-services-06a743bb-ceb6-49a9-a61d-db4ffdf54fa6
+Also Need Online Sign in assistant
+https://www.microsoft.com/en-us/download/details.aspx?id=39267
+
+Use Case:  Low Trust Provider Hosted Apps or Add-ins require Client Secret, which are valid for either 1 or 3 years. Once they Expire, 
+the apps will stop working and you need to generate new once for that particular appId or clientId. This PowerShell Script connects 
+to Azure Active Directory and dumps all the un expired and expired/about to expire secrets and it keys. 
+The appId and the secret together is sensitive information so please keep them secure and do not share clientsecret to any unauthorized users. 
+
+Usage:
+Open Windows Powershell in administrative mode and cd to the location where you save this script.
+Run the script. It will prompt for your onmicrosoft account and password. Once authentication succeeds 
+It will create 2 files in the same folder as below:-
+Secret_Valid.txt: All unexpired and valid secrets are in Secret_Valid.
+Secret_Expiring.txt: All expired and about to be expiring (10 days from the day you run this script).
+
+#> 
+
 
 
 Import-module MSOnline -ErrorAction Continue
